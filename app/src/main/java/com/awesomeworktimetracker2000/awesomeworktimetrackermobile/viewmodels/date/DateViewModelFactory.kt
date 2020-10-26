6ed5f771b6c02dfd127a875b8ddbf14e7bda9545
iojani <8681846+iojani@users.noreply.github.com>
2020-10-26 19:40:15 +0200

    created DateViewModel, DateViewModelFactory

--- app/src/main/java/com/awesomeworktimetracker2000/awesomeworktimetrackermobile/viewmodels/date/DateViewModelFactory.kt
+++ app/src/main/java/com/awesomeworktimetracker2000/awesomeworktimetrackermobile/viewmodels/date/DateViewModelFactory.kt
@@ -3,6 +3,7 @@ package com.awesomeworktimetracker2000.awesomeworktimetrackermobile.viewmodels.d
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+<<<<<<< HEAD
 import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.database.AWTDatabase
 import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.database.daos.UserInfoDao
 import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.database.daos.WorktimeEntryDao
@@ -12,19 +13,34 @@ import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.reposito
 import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.repositories.WorktimeEntryRepository
 import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.utils.ConnectionUtils
 import kotlinx.coroutines.runBlocking
+=======
+import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.database.daos.UserInfoDao
+import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.network.services.AWTApiService
+import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.repositories.UserRepository
+import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.utils.ConnectionUtils
+>>>>>>> created DateViewModel, DateViewModelFactory
 import java.util.*
 
 @Suppress("UNCHECKED_CAST")
 class DateViewModelFactory (
+<<<<<<< HEAD
     private val worktimeEntryDao: WorktimeEntryDao,
     private val userInfoDao: UserInfoDao,
     private val apiService: AWTApiService,
     private val connectionUtils: ConnectionUtils
 
+=======
+    private val date: Date,
+    private val userInfoDao: UserInfoDao,
+    private val apiService: AWTApiService,
+    private val connectionUtils: ConnectionUtils,
+    private val application: Application
+>>>>>>> created DateViewModel, DateViewModelFactory
 ): ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(DateViewModel::class.java)) {
+<<<<<<< HEAD
             var weRepo: WorktimeEntryRepository
 
             runBlocking {
@@ -37,6 +53,9 @@ class DateViewModelFactory (
             }
             return DateViewModel(weRepo) as T
 
+=======
+            return DateViewModel(date, UserRepository.getInstance(userInfoDao, apiService, connectionUtils), application) as T
+>>>>>>> created DateViewModel, DateViewModelFactory
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
