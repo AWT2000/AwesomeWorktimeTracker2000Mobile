a0cd8641ee00b5c33b9fa7e58cc467956416fef0
iojani <8681846+iojani@users.noreply.github.com>
2020-11-01 15:47:10 +0200

    created DateViewModel, DateViewModelFactory

--- app/src/main/java/com/awesomeworktimetracker2000/awesomeworktimetrackermobile/viewmodels/date/DateViewModel.kt
+++ app/src/main/java/com/awesomeworktimetracker2000/awesomeworktimetrackermobile/viewmodels/date/DateViewModel.kt
@@ -1,68 +1,73 @@
 package com.awesomeworktimetracker2000.awesomeworktimetrackermobile.viewmodels.date
 
-import android.app.Application
 import android.util.Log
-import androidx.lifecycle.*
-import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.database.AWTDatabase
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.models.WorktimeEntry
-import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.network.services.AWTApi
-import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.repositories.UserRepository
 import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.repositories.WorktimeEntryRepository
 import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.repositories.wrappers.ResponseStatus
-import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.repositories.wrappers.WorktimeEntryListing
-import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.utils.ConnectionUtils
-import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.utils.DateUtils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.ZoneId
 import java.util.*
 
-// alternatively use AndroidViewModel as Application context aware ViewModel
-class DateViewModel(date: Date, private val userRepository: UserRepository, private val application: Application) : ViewModel() {
+class DateViewModel(private val worktimeEntryRepository: WorktimeEntryRepository) : ViewModel() {
 
-    //val sdf = SimpleDateFormat("yyyy-mm-dd")
+    //val sdf = SimpleDateFormat("yyyy-MM-dd")
 
-    // TODO: Databinding, getters for Mutable Data?
-    private val date = MutableLiveData<Date>(date)
-    private lateinit var _worktimeEntries : MutableLiveData<List<WorktimeEntry>>
+    // Placeholder until date gets passed from week vm
+    private var inputDate: LocalDate = LocalDate.of(2020, 4, 15)
+    private var date: Date = Date.from(inputDate.atStartOfDay(ZoneId.systemDefault()).toInstant())
+    //private val date = MutableLiveData<Date>()
 
+    // For updating DateFragment text view to current date
+    var currentDate = MutableLiveData<String>()
+    private var finnish: Locale? = Locale("fi", "FI")
+    private val sdf = SimpleDateFormat("EE dd.MM.yyyy", finnish)
+
+    // Observed in DateFragment
+    var worktimeEntries = MutableLiveData<List<WorktimeEntry>>()
 
     init {
-        getWorkTimeEntries(date)
         Log.i("DateViewModel", "DateViewModel created")
+        // Placeholder until date gets passed from week vm
+        getWorkTimeEntries(date)
     }
 
-    private fun getWorkTimeEntries(date : Date) {
-        var weRepo: WorktimeEntryRepository
 
-        runBlocking {
-            val db = AWTDatabase.getInstance(application)
-
-            weRepo = WorktimeEntryRepository.getInstance(
-                AWTApi.service,
-                db.worktimeEntryDao,
-                db.userDao,
-                ConnectionUtils.getInstance(application)
-            )
-        }
+    private fun getWorkTimeEntries(date: Date) {
         viewModelScope.launch(Dispatchers.IO) {
-            val entryListing = weRepo.getWorktimeEntriesByDate(date)
+            val entryListing = worktimeEntryRepository.getWorktimeEntriesByDate(date)
+            Log.i("entryListing", entryListing.worktimeEntries?.count().toString())
+            Log.i("entryListing", entryListing.status.toString())
+            Log.i("date", date.toString())
+            if (entryListing.status == ResponseStatus.OK) {
+                currentDate.postValue(sdf.format(date))
+            }
 
-            _worktimeEntries.value = entryListing.worktimeEntries
-
-            if (entryListing.status == ResponseStatus.OK && entryListing.worktimeEntries != null) {
-                val entries = entryListing.worktimeEntries
-                if (entries.count() == 0) {
-                    Log.i("worktimeEntries", "entries.count() == 0")
-                }
-
-                entries.forEach { entry ->
-                    Log.i("worktimeEntries", "external id: ${entry.externalId}, started_at: "
-                            + "${entry.startedAt.format(DateUtils.isoDateFormatter)}, "
-                            + "ended_at: ${entry.endedAt.format(DateUtils.isoDateFormatter)}" )
-                }
+            if (entryListing.worktimeEntries != null) {
+                worktimeEntries.postValue(entryListing.worktimeEntries)
             }
         }
     }
+
+
+    /* TODO: in case call to worktimeEntryRepository fails with nextDate() or prevDate(), pass some temporary date object instead,
+       and update the date LiveData object only after successful response
+     */
+    fun nextDate() {
+        inputDate = inputDate.plusDays(1)
+        date = Date.from(inputDate.atStartOfDay(ZoneId.systemDefault()).toInstant())
+        getWorkTimeEntries(date)
+    }
+
+    fun prevDate() {
+        inputDate = inputDate.minusDays(1)
+        date = Date.from(inputDate.atStartOfDay(ZoneId.systemDefault()).toInstant())
+        getWorkTimeEntries(date)
+    }
+
 }
--- app/src/main/java/com/awesomeworktimetracker2000/awesomeworktimetrackermobile/viewmodels/date/DateViewModelFactory.kt
+++ app/src/main/java/com/awesomeworktimetracker2000/awesomeworktimetrackermobile/viewmodels/date/DateViewModelFactory.kt
@@ -3,24 +3,40 @@ package com.awesomeworktimetracker2000.awesomeworktimetrackermobile.viewmodels.d
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.database.AWTDatabase
 import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.database.daos.UserInfoDao
+import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.database.daos.WorktimeEntryDao
+import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.network.services.AWTApi
 import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.network.services.AWTApiService
 import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.repositories.UserRepository
+import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.repositories.WorktimeEntryRepository
 import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.utils.ConnectionUtils
+import kotlinx.coroutines.runBlocking
 import java.util.*
 
 @Suppress("UNCHECKED_CAST")
 class DateViewModelFactory (
-    private val date: Date,
+    private val worktimeEntryDao: WorktimeEntryDao,
     private val userInfoDao: UserInfoDao,
     private val apiService: AWTApiService,
-    private val connectionUtils: ConnectionUtils,
-    private val application: Application
+    private val connectionUtils: ConnectionUtils
+
 ): ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(DateViewModel::class.java)) {
-            return DateViewModel(date, UserRepository.getInstance(userInfoDao, apiService, connectionUtils), application) as T
+            var weRepo: WorktimeEntryRepository
+
+            runBlocking {
+                weRepo = WorktimeEntryRepository.getInstance(
+                    apiService,
+                    worktimeEntryDao,
+                    userInfoDao,
+                    connectionUtils
+                )
+            }
+            return DateViewModel(weRepo) as T
+
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
