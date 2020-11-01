5371842e16a27131788879343822bb15f72a9a26
iojani <8681846+iojani@users.noreply.github.com>
2020-11-01 15:48:15 +0200

    add databinding, onclicklisteners

--- app/src/main/java/com/awesomeworktimetracker2000/awesomeworktimetrackermobile/ui/date/DateFragment.kt
+++ app/src/main/java/com/awesomeworktimetracker2000/awesomeworktimetrackermobile/ui/date/DateFragment.kt
@@ -1,14 +1,28 @@
 package com.awesomeworktimetracker2000.awesomeworktimetrackermobile.ui.date
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.R
+import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.database.AWTDatabase
+import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.data.network.services.AWTApi
+import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.databinding.DateFragmentBinding
+import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.utils.ConnectionUtils
+import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.utils.DateUtils
+import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.viewmodels.date.DateViewModel
+import com.awesomeworktimetracker2000.awesomeworktimetrackermobile.viewmodels.date.DateViewModelFactory
+import kotlinx.android.synthetic.main.date_fragment.*
 
 
 class DateFragment : Fragment() {
+
+    private lateinit var binding: DateFragmentBinding
 
     companion object {
         fun newInstance() = DateFragment()
@@ -25,8 +39,43 @@ class DateFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.date_fragment, container, false)
+        // initialize data binding
+        binding = DataBindingUtil.inflate(inflater, R.layout.date_fragment, container, false)
+        binding.lifecycleOwner = this
+
+        val application = requireNotNull(this.activity).application
+
+        // initialize view model
+        val vmf = DateViewModelFactory(
+            AWTDatabase.getInstance(application).worktimeEntryDao,
+            AWTDatabase.getInstance(application).userDao,
+            AWTApi.service,
+            ConnectionUtils.getInstance(application)
+        )
+
+        val vm = ViewModelProvider(this, vmf).get(DateViewModel::class.java)
+
+        vm.worktimeEntries.observe(viewLifecycleOwner, Observer {
+            it.forEach { entry ->
+                Log.i("worktimeEntries", "external id: ${entry.externalId}, started_at: "
+                        + "${entry.startedAt.format(DateUtils.isoDateFormatter)}, "
+                        + "ended_at: ${entry.endedAt.format(DateUtils.isoDateFormatter)}" )
+            }
+        })
+
+        // Observe currentDate in DateViewModel and update tvDate.text accordingly
+        vm.currentDate.observe(viewLifecycleOwner, Observer {
+            this.tvDate.text = vm.currentDate.value.toString()
+        })
+
+        binding.btnNextDate.setOnClickListener {
+            vm.nextDate()
+        }
+        binding.btnPrevDate.setOnClickListener {
+            vm.prevDate()
+        }
+
+        return binding.root
     }
 
 
